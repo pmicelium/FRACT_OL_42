602a960df90500df7b3f172c7226d0ee26a78fd4
Pierre MICELI <pmiceli@e1r3p6.42.fr>
2018-01-23 05:57:56 +0100

    forgot a destroy_image

--- srcs/key_fonct.c
+++ srcs/key_fonct.c
@@ -6,7 +6,7 @@
 /*   By: pmiceli <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 23:51:54 by pmiceli           #+#    #+#             */
-/*   Updated: 2018/01/23 03:36:24 by pmiceli          ###   ########.fr       */
+/*   Updated: 2018/01/23 05:57:18 by pmiceli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int				key_fonct(int keycode, t_f *f)
 	if (keycode == KEY_ESC)
 	{
 		ft_putendl_color("exiting ...", "green");
+		if (f->flags.map == 0)
+			display_choice(f, 2);
 		mlx_clear_window(f->mlx.ptr, f->mlx.win);
 		mlx_destroy_window(f->mlx.ptr, f->mlx.win);
 		exit(1);
