696fc7f85e4052a94b3224ddc7db4a6d5a84c753
Pierre MICELI <pmiceli@e1r8p17.42.fr>
2018-03-10 06:43:07 +0100

    norme srcs

--- srcs/draw/draw_map.c
+++ srcs/draw/draw_map.c
@@ -6,12 +6,40 @@
 /*   By: pmiceli <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 00:29:19 by pmiceli           #+#    #+#             */
-/*   Updated: 2018/03/10 05:10:18 by pmiceli          ###   ########.fr       */
+/*   Updated: 2018/03/10 06:42:42 by pmiceli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract.h"
 
+void			draw_map_2(t_f *f, int fract)
+{
+	if (fract == HUGULUS)
+	{
+		if (f->flags.hugulus_repaint == 0)
+			draw_hugulus(f, NEW);
+		else
+			draw_hugulus(f, REPAINT);
+	}
+	else if (fract == VOID)
+	{
+		if (f->flags.display_repaint == 0)
+			display_choice(f, NEW);
+		else
+			display_choice(f, REPAINT);
+	}
+/*
+	else if (fract == TREE)
+	{
+		if (f->flags.tree_repaint == 0)
+			draw_tree(f, NEW);
+		else
+			draw_tree(f, REPAINT);
+	}
+*/
+}
+// sur julia et ship => voir pour le repaint a chaque ite//
+//
 void			draw_map(t_f *f, int fract)
 {
 	if (fract == MANDEL)
@@ -35,25 +63,7 @@ void			draw_map(t_f *f, int fract)
 		else
 			draw_ship(f, REPAINT);
 	}
-	else if (fract == HUGULUS)
-	{
-		if (f->flags.hugulus_repaint == 0)
-			draw_hugulus(f, NEW);
-		else
-			draw_hugulus(f, REPAINT);
-	}
-	else if (fract == VOID)
-	{
-		if (f->flags.display_repaint == 0)
-			display_choice(f, NEW);
-		else
-			display_choice(f, REPAINT);
-	}
-//	else if (fract == TREE)
-//	{
-//		if (f->flags.tree_repaint == 0)
-//			draw_tree(f, NEW);
-//		else
-//			draw_tree(f, REPAINT);
-//	}
+	else
+		draw_map_2(f, fract);
 }
+
